69ad744b949f6e1f6c6b26134aa446cbef19407e
PeAcePh1NiX <savage89100@gmail.com>
2024-09-26 22:25:01 +0545

    1.1

--- src/utilities/rootpath/path.utils.ts
+++ src/utilities/rootpath/path.utils.ts
@@ -1,6 +1,6 @@
 import { join, resolve } from 'path';
 
-const rootPath = resolve(__dirname, '../../'); // Adjust the number of '../' to get to the root directory
+const rootPath = resolve(__dirname, '../../../'); // Adjust this based on your folder structure
 
 export const getRootPath = (...args) => {
   return join(rootPath, ...args);
